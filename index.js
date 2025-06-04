1826fab7569836932c358f27c555e207ab7fe935
valaykamol <vlykamol@outlook.com>
2025-06-04 23:58:49 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const dotenv = require('dotenv')
+dotenv.config()
+
+port = process.env.PORT | 8080
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send("hello world!")
+})
+
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+})
