14d496d9f7c64b6f69509c4d27391096769187df
valaykamol <vlykamol@outlook.com>
2025-06-05 16:15:32 +0530

    routes

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express')
-
 const dotenv = require('dotenv')
 dotenv.config()
+
+// routes
+
+const userRoutes = require('./routes/userRoutes')
 
 port = process.env.PORT | 8080
 const app = express()
 
 app.use(express.json())
+
+app.use('/user', userRoutes)
 
 app.get('/', (req, res) => {
   res.send("hello world!")
